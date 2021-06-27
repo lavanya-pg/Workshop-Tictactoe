07516e613fb6d9d9d9551a332577baf3673c4bd2
lavanya-pg <lavanyapg8@gmail.com>
2021-06-27 23:00:32 +0530

    ADDED:Choose a Letter

--- workshop/Tic.java
+++ workshop/Tic.java
@@ -1,11 +1,30 @@
 package com.workshop;
 
+import java.util.Scanner;
+
 public class Tic
 {
 	public static void main(String[] args)
 	{
 		System.out.println("welcome to the tic-tac-toe program");
-
-	}
-
+		System.out.println("Choose a letter o or x : ");
+        Scanner s = new Scanner(System.in);
+        char letter = s.next().charAt(0);
+        char Choice;
+        chooseLetter(letter);
+     }
+		public static void  chooseLetter(char letter)
+		{
+		switch(letter)
+		{
+		 case 'o':
+		        System.out.println("player entered letter: o");
+		        System.out.println("computer choice is: x");
+		        break;
+		 case 'x':
+		        System.out.println("player entered letter: x");
+		        System.out.println("computer choice is: o");
+		        break;
+		 }
+		 }
 }
